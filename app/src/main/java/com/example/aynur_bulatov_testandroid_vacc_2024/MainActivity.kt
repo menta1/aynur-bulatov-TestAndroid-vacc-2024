7c6a5beb45ff00842e068acaae9383151a87bd49
ainur <exen1993@gmail.com>
2024-05-24 03:15:26 +0500

    navigation

--- app/src/main/java/com/example/aynur_bulatov_testandroid_vacc_2024/MainActivity.kt
+++ app/src/main/java/com/example/aynur_bulatov_testandroid_vacc_2024/MainActivity.kt
@@ -3,20 +3,33 @@ package com.example.aynur_bulatov_testandroid_vacc_2024
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.example.aynur_bulatov_testandroid_vacc_2024.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.containerView) as NavHostFragment
+
+        navController = navHostFragment.navController
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+
+            when (destination.id) {
+                R.id.mainScreenFragment -> {}
+                R.id.authFragment -> {}
+                R.id.detailsScreenFragment -> {}
+                R.id.shoppingCardFragment -> {}
+            }
         }
     }
 }
